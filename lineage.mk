687205daf55e10201558261cff100143a8ebe50a
nsingh94 <namit1994@gmail.com>
2021-01-12 15:22:42 -0500

    Reorganize device tree .mk files

--- lineage.mk
+++ lineage.mk
@@ -1,5 +1,16 @@
-# Inherit device configuration
-$(call inherit-product, device/amazon/suez/full_suez.mk)
+# Inherit from the common Open Source product configuration
+$(call inherit-product-if-exists, vendor/lineage/config/common_full_tablet_wifionly.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_l.mk)
 
+# Inherit from hardware-specific part of the product configuration
+$(call inherit-product, device/amazon/suez/device_suez.mk)
+
+PRODUCT_DEVICE := suez
 PRODUCT_NAME := lineage_suez
-PRODUCT_RELEASE_NAME := suez
+PRODUCT_BRAND := Amazon
+PRODUCT_MODEL := Fire HD 10
+PRODUCT_MANUFACTURER := Amazon
+PRODUCT_CHARACTERISTICS := tablet
+PRODUCT_RESTRICT_VENDOR_FILES := false
