ddc98157f69dc230cf083259d94635045b38558e
nsingh94 <namit1994@gmail.com>
2021-03-01 12:28:07 -0500

    Update ui_shim for stock omx lib

--- mediatek/libshims/ui/ui_shim.cpp
+++ mediatek/libshims/ui/ui_shim.cpp
@@ -15,6 +15,8 @@
  */
 
 #include <string>
+#include <ui/GraphicBufferMapper.h>
+#include <ui/Rect.h>
 
 // GraphicBuffer(uint32_t inWidth, uint32_t inHeight, PixelFormat inFormat,
 //               uint32_t inUsage, std::string requestorName = "<Unknown>");
@@ -27,4 +29,12 @@ extern "C" void _ZN7android13GraphicBufferC1Ejjij(
   std::string requestorName = "<Unknown>";
   _ZN7android13GraphicBufferC1EjjijNSt3__112basic_stringIcNS1_11char_traitsIcEENS1_9allocatorIcEEEE(
       inWidth, inHeight, inFormat, inUsage, requestorName);
+}
+
+extern "C" {
+    void _ZN7android19GraphicBufferMapper4lockEPK13native_handlejRKNS_4RectEPPv(buffer_handle_t, uint32_t, const android::Rect&, void**);
+
+    void _ZN7android19GraphicBufferMapper4lockEPK13native_handleiRKNS_4RectEPPv(buffer_handle_t handle, int usage, const android::Rect& bounds, void** vaddr) {
+        _ZN7android19GraphicBufferMapper4lockEPK13native_handlejRKNS_4RectEPPv(handle, static_cast<uint32_t>(usage), bounds, vaddr);
+    }
 }
